5a5cd65afa9a8e4f68ab72d6b080993da74a21be
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2025-10-01 13:40:27 +0300

    [echo-server] Return hostname of server

--- echo-server/docker/echo-server.go
+++ echo-server/docker/echo-server.go
@@ -1,31 +1,37 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"time"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "time"
+    "os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	// Dump headers to JSON for response
-	headers := map[string][]string(r.Header)
-	response := map[string]interface{}{
-		"method":  r.Method,
-		"url":     r.URL.String(),
-		"headers": headers,
-		"remote":  r.RemoteAddr,
-	}
+var HOSTNAME string
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+func handler(w http.ResponseWriter, r *http.Request) {
+    // Dump headers to JSON for response
+    headers := map[string][]string(r.Header)
+    response := map[string]interface{}{
+        "server": HOSTNAME,
+        "method": r.Method,
+        "url": r.URL.String(),
+        "headers": headers,
+        "remote": r.RemoteAddr,
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(response)
 
     // Print access log
-	fmt.Printf("[%s] %s -- %s %s\n", time.Now().Format(time.RFC3339), r.RemoteAddr, r.Method, r.URL.Path)
+    fmt.Printf("[%s] %s %s -- %s %s\n",
+      time.Now().Format(time.RFC3339), HOSTNAME, r.RemoteAddr, r.Method, r.URL.Path)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Starting echo server on :8080")
-	http.ListenAndServe(":8080", nil)
+    HOSTNAME, _ = os.Hostname()
+    http.HandleFunc("/", handler)
+    fmt.Println("Starting echo server on :8080")
+    http.ListenAndServe(":8080", nil)
 }
